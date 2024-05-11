9a7a725986891f6ffdbd747152beb49a0e92754b
Adib Pathan <121228160+adibpathan@users.noreply.github.com>
2024-05-11 20:34:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import UserProfile from './UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
